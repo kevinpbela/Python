04c549291b4228c3d400894deb4244e0102b5ed1
eduardobaltazarmarfim <66966652+eduardobaltazarmarfim@users.noreply.github.com>
2020-06-16 20:26:40 -0300

    Update pintar_parede.py

--- Excercícios/pintar_parede.py
+++ Excercícios/pintar_parede.py
@@ -21,8 +21,10 @@ def verificar():
         largura=float(input('Digite a largura: '))
         comprimento=float(input('Digite o comprimento: '))
 
+        calcular(largura,comprimento)
 
-
+        retorno()
+        
     except:
 
         print('Erro - Dados digitados são invalidos!')
@@ -35,9 +37,9 @@ def calcular(x,y):
 
     res=x*y
 
-    print('Sua parede tem dimensão de {}x`{} e sua área é de {}m².'.format(x,y,res))
+    print('Sua parede tem dimensão de {:.2f}x{:.2f} e sua área é de {:.2f}m².'.format(x,y,res))
 
-    print('Para pintar sua parede você vai ')
+    print('Para pintar sua parede você vai precisar de {:.2f}L de tinta por m².'.format(res/2))
 
     pass
 
