fede300aa5f8637d3139f3c5b19047ae91e544b7
eduardobaltazarmarfim <66966652+eduardobaltazarmarfim@users.noreply.github.com>
2020-06-29 22:08:15 -0300

    Update sistema.py

--- Tkinter/Projeto Pratico/sistema.py
+++ Tkinter/Projeto Pratico/sistema.py
@@ -9,16 +9,26 @@ class Aplicacao():
         self.janela=janela
         self.tela()
         self.frames_de_tela()
+        self.botoes()
         janela.mainloop()
 
         pass
 
     def tela(self):
 
+        largura=950
+        altura=500
+
+        largura_tela=self.janela.winfo_screenwidth()
+        altura_tela=self.janela.winfo_screenheight()
+
+        posx=(largura_tela/2)-(largura/2)
+        posy=(altura_tela/2)-(altura/2)
+
         self.janela.title("Cadastro de Cliente")
         self.janela['bg']='#30336b'
-        self.janela.geometry('750x500+0+0')
-        self.janela.resizable(True,True)
+        self.janela.geometry('%dx%d+%d+%d'%(largura,altura,posx,posy))
+        self.janela.resizable(False,False)
         self.janela.maxsize(width=900,height=700)
         self.janela.minsize(width=400,height=300)
 
@@ -30,7 +40,23 @@ class Aplicacao():
         self.frame_1.place(relx=0.02,rely=0.02,relwidth=0.96,relheight=0.45)
 
         self.frame_2= Frame(self.janela)
-        self.frame_2.place(relx=0.02,rely=0.02,relwidth=0.96,relheight=0.45)
+        self.frame_2.place(relx=0.02,rely=0.5,relwidth=0.96,relheight=0.45)
+
+        pass
+
+    def botoes(self):
+
+        self.btnlimpar=Button(self.frame_1,text='Limpar')
+        self.btnlimpar.place(relx=0.01,rely=0.05,relwidth=0.15,relheight=0.20)
+
+        self.btnlimpar=Button(self.frame_1,text='Consultar')
+        self.btnlimpar.place(relx=0.01,rely=0.28,relwidth=0.15,relheight=0.20)
+
+        self.btnlimpar=Button(self.frame_1,text='Novo')
+        self.btnlimpar.place(relx=0.01,rely=0.52,relwidth=0.15,relheight=0.20)
+
+        self.btnlimpar=Button(self.frame_1,text='Salvar')
+        self.btnlimpar.place(relx=0.01,rely=0.74,relwidth=0.15,relheight=0.20)
 
         pass
 
