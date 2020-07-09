46e099b614de78f3944c209fd41818d3fbc58f3e
eduardobaltazarmarfim <66966652+eduardobaltazarmarfim@users.noreply.github.com>
2020-07-08 22:43:06 -0300

    atualizado

--- Tkinter/Projeto Pratico/sistema.py
+++ Tkinter/Projeto Pratico/sistema.py
@@ -1,65 +1,32 @@
 from tkinter import*
 
+cor='#dde'
+
 janela=Tk()
 
-class Aplicacao():
+janela.title('form01')
+janela.geometry('250x150+0+0')
+janela['bg']=cor
 
-    def __init__(self):
+lbvalor01=Label(janela,text='Valor 01:',background=cor)
+lbvalor02=Label(janela,text='Valor 02:',background=cor)
 
-        self.janela=janela
-        self.tela()
-        self.frames_de_tela()
-        self.botoes()
-        janela.mainloop()
+lbvalor01.place(x=10,y=10)
+lbvalor02.place(x=10,y=45)
 
-        pass
+txvl01=Entry(janela).place(x=65,y=10)
+txvl02=Entry(janela).place(x=65,y=45)
 
-    def tela(self):
+btnsoma=Button(janela,text='Somar',command=lambda:print(txvl01.get())).place(x=65,y=75)
 
-        largura=950
-        altura=500
+lbvalor03=Label(janela,text='Mensagem',width=25,background=cor)
+lbvalor03.place(x=25,y=120)
 
-        largura_tela=self.janela.winfo_screenwidth()
-        altura_tela=self.janela.winfo_screenheight()
+janela.mainloop()
 
-        posx=(largura_tela/2)-(largura/2)
-        posy=(altura_tela/2)-(altura/2)
 
-        self.janela.title("Cadastro de Cliente")
-        self.janela['bg']='#30336b'
-        self.janela.geometry('%dx%d+%d+%d'%(largura,altura,posx,posy))
-        self.janela.resizable(False,False)
-        self.janela.maxsize(width=900,height=700)
-        self.janela.minsize(width=400,height=300)
+def soma(a,b):
 
-        pass
-
-    def frames_de_tela(self):
-
-        self.frame_1= Frame(self.janela)
-        self.frame_1.place(relx=0.02,rely=0.02,relwidth=0.96,relheight=0.45)
-
-        self.frame_2= Frame(self.janela)
-        self.frame_2.place(relx=0.02,rely=0.5,relwidth=0.96,relheight=0.45)
-
-        pass
-
-    def botoes(self):
-
-        self.btnlimpar=Button(self.frame_1,text='Limpar')
-        self.btnlimpar.place(relx=0.01,rely=0.05,relwidth=0.15,relheight=0.20)
-
-        self.btnlimpar=Button(self.frame_1,text='Consultar')
-        self.btnlimpar.place(relx=0.01,rely=0.28,relwidth=0.15,relheight=0.20)
-
-        self.btnlimpar=Button(self.frame_1,text='Novo')
-        self.btnlimpar.place(relx=0.01,rely=0.52,relwidth=0.15,relheight=0.20)
-
-        self.btnlimpar=Button(self.frame_1,text='Salvar')
-        self.btnlimpar.place(relx=0.01,rely=0.74,relwidth=0.15,relheight=0.20)
-
-        pass
+    return a+b
 
     pass
-
-Aplicacao()
